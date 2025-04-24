cd61bc5d62f47b0443f916d81704782938e8b6e2
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-24 15:02:41 +0530

    Lecture 77

--- Demo.java
+++ Demo.java
@@ -1,46 +1,19 @@
-interface A{
-    // All the variables inside interface are final and static
-
-    int age=23;
-    String area="Mumbai";
-
-    void show();
-    void config();
-    // even if we dont mention methods to be abstract they are public abstract by default in an interface.
-}
-interface X{
-
-    void run();
+enum Status{
+    Running, Failed, Success,Pending;
 }
 
-interface Y extends X{
-
-}
-class B implements A,Y{
-    public void show(){
-        System.out.println("In show");
-    }
-
-    public void config(){
-        System.out.println("In config");
-    }
-
-    public void run(){
-        System.out.println("Running..");
-    }
-}
-public class Demo{
+class Demo{
     public static void main(String[] args) {
-       A obj;
-    //    obj= new A();
-    obj = new B();
-    obj.show();
-    obj.config();
+        // Status s= Status.Running;
+        // If we want to get all the values then we will use values() method
+        Status ss[] = Status.values();
+        // System.out.println(ss[1]);
 
-    // obj.run(); //error
-    X obj1;
-    obj1= new B();
-    obj1.run();
-    // System.out.println(A);
+        for(Status s : ss){
+            System.out.println(s);
+        }
+        // System.out.println(s);
+        // System.out.println(s.ordinal()); // This method returns a  number associated with an enum item
+
     }
 }
