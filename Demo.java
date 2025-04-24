36538f728aed9d244daef0dcd525565cf0c82935
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-24 19:11:38 +0530

    Lecture 84

--- Demo.java
+++ Demo.java
@@ -1,21 +1,13 @@
 @FunctionalInterface
 interface  A{
-    void show(int i);
-    // void run();
+    int add(int i, int j);
 }
 
-// class B implements  A{
-//     public void show(){
-//         System.out.println("In show");
-//     }
-// }
 public class Demo{
     public static void main(String args[]){
-       A obj= i -> 
-    //    {   // -> means that it is a lamda expression
+        A obj = ( i, j) ->   i+j; // if we have one return value then we dont need to add the return statement the return value will be the type of i+j
 
-                System.out.println("In show");
-            // };
-       obj.show(5);
+        int result= obj.add(5,4);
+        System.out.println(result);
        }
     }
