0082d0f70e8941ce0d994d2244b54e9bb7dcef85
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-26 18:54:19 +0530

    Lecture 98

--- Demo.java
+++ Demo.java
@@ -1,70 +1,36 @@
-
-// class A implements  Runnable 
-//  {
-//     public void run(){
-
-//         for(int i=1;i<=5;i++)
-//         {
-//             System.out.println("Hi");
-//             try {
-//                 Thread.sleep(10); 
-//                             } catch (InterruptedException ex) {
-//                 ex.printStackTrace();
-//             }
-//         }
-//     }
-// }
-
-// class B implements Runnable{
-//     public void run(){
-
-//         for(int i=1;i<=10;i++){
-//         System.out.println("Hello");
-//         try {
-//             Thread.sleep(10);
-//         } catch (InterruptedException e) {
-//             e.printStackTrace();
-//         }
-
-//         }
-//     }
-// }
+class Counter{
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
 
 public class Demo{
-    public static void main(String args[]){
-        // A obj1= new A();
-        // B obj2= new B();
-        // Runnable obj1= new A();
+    public static void main(String args[]) throws InterruptedException{
+        Counter c= new Counter();
+
         Runnable obj1= () -> {
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<=10000;i++)
         {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10); 
-                            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
+            c.increment();
         }
     };
-        // Runnable obj2= new B();
         Runnable obj2= () -> {
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<=10000;i++)
         {
-            System.out.println("Hello");
-            try {
-                Thread.sleep(10); 
-                            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
+            c.increment();
         }
     };
-
-        // The runnable interface does not have methods of the thread class so we have to create seperate threads and use the methods
-
-        // obj2.start(); // Here start will not work as it is the part of Thread class so to solve this problem,
-        Thread t1= new Thread(obj1);
+       Thread t1= new Thread(obj1);
         Thread t2= new Thread(obj2);
         t1.start();
         t2.start();
+
+        // join method allows the threads to complete and join the main thread
+        t1.join();
+        t2.join();
+        
+        System.out.println(c.count); // Here the main thread is not waiting for the t1 and t2 threads to wait for them to finish
+
        }    
     }
