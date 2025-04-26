79542156996ea713dd60987b171aeb3445ece199
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-26 18:33:53 +0530

    Lecture 97

--- Demo.java
+++ Demo.java
@@ -1,53 +1,70 @@
 
-import com.sun.jdi.event.ThreadStartEvent;
+// class A implements  Runnable 
+//  {
+//     public void run(){
 
-class A extends Thread // Now this class is not an ordinary class it is a thread
- {
-    public void run(){
+//         for(int i=1;i<=5;i++)
+//         {
+//             System.out.println("Hi");
+//             try {
+//                 Thread.sleep(10); 
+//                             } catch (InterruptedException ex) {
+//                 ex.printStackTrace();
+//             }
+//         }
+//     }
+// }
 
-        for(int i=1;i<=10;i++)
-        {
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10); // If we are specifying this then we are asking our thread to wait for the number of milliseconds
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-}
+// class B implements Runnable{
+//     public void run(){
 
-class B extends Thread{
-    public void run(){
+//         for(int i=1;i<=10;i++){
+//         System.out.println("Hello");
+//         try {
+//             Thread.sleep(10);
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
 
-        for(int i=1;i<=10;i++){
-        System.out.println("Hello");
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        }
-    }
-}
+//         }
+//     }
+// }
 
 public class Demo{
     public static void main(String args[]){
-        A obj1= new A();
-        B obj2= new B();
-
-        obj2.setPriority(Thread.MAX_PRIORITY); // This is the constant which has the value 10
-        obj1.start(); // We need to give start if we want to implement threads and the method name will be run()
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
+        // A obj1= new A();
+        // B obj2= new B();
+        // Runnable obj1= new A();
+        Runnable obj1= () -> {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(10); 
+                            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
-        obj2.start();
-         // This method is used to give the priority of the threads
-        System.out.println(obj1.getPriority());
-        System.out.println(obj2.getPriority());
+    };
+        // Runnable obj2= new B();
+        Runnable obj2= () -> {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(10); 
+                            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    };
 
+        // The runnable interface does not have methods of the thread class so we have to create seperate threads and use the methods
+
+        // obj2.start(); // Here start will not work as it is the part of Thread class so to solve this problem,
+        Thread t1= new Thread(obj1);
+        Thread t2= new Thread(obj2);
+        t1.start();
+        t2.start();
        }    
     }
