4a0dcdd658a367d36bcc95ffc780434ba8734221
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-26 19:34:49 +0530

    Lecture 102

--- Demo.java
+++ Demo.java
@@ -1,7 +1,12 @@
 
+import java.awt.font.NumericShaper;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Demo{
     public static void main(String[] args) {
@@ -9,18 +14,32 @@ public class Demo{
         // Lists support index values
 
         // Collection<Integer> nums= new ArrayList<Integer>();
-        List<Integer> nums= new ArrayList<Integer>();
+        // List<Integer> nums= new ArrayList<Integer>();
+        // Set <Integer> nums= new HashSet<Integer>(); // Sets will eliminate the duplicates
+
+        // If we want a set with sorted values we will use TreeSet()
+
+        Set<Integer> nums= new TreeSet<Integer>();
+
         nums.add(6); // These values are not numbers they are object types so to use numbers we need to specify the datatype in 
         // angular brackets
 
         nums.add(7);
         nums.add(2);
         nums.add(1);
+        nums.add(6); // Lists support multiple values
+        // Sets do not have index values
 
+
+        Iterator<Integer> values= nums.iterator(); 
+
+        while(values.hasNext()){
+            System.out.println(values.next());
+        }
         // for(int i: nums){
         //     System.out.println(i);
         // }
-        System.out.println(nums.get(1));
+        // System.out.println(nums.get(1));
         // System.out.println(nums); // We cannot directly print the original arrays but here it is possible
     }
 }
