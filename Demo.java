162656c9a8693e33a16a419869e818bc0cfa4ac0
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-26 15:25:02 +0530

    Lecture 96

--- Demo.java
+++ Demo.java
@@ -1,17 +1,34 @@
+
+import com.sun.jdi.event.ThreadStartEvent;
+
 class A extends Thread // Now this class is not an ordinary class it is a thread
  {
     public void run(){
 
         for(int i=1;i<=10;i++)
-        System.out.println("Hi");
+        {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(10); // If we are specifying this then we are asking our thread to wait for the number of milliseconds
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
 
 class B extends Thread{
     public void run(){
 
-        for(int i=1;i<=10;i++)
+        for(int i=1;i<=10;i++){
         System.out.println("Hello");
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        }
     }
 }
 
@@ -20,7 +37,17 @@ public class Demo{
         A obj1= new A();
         B obj2= new B();
 
+        obj2.setPriority(Thread.MAX_PRIORITY); // This is the constant which has the value 10
         obj1.start(); // We need to give start if we want to implement threads and the method name will be run()
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         obj2.start();
-       }
+         // This method is used to give the priority of the threads
+        System.out.println(obj1.getPriority());
+        System.out.println(obj2.getPriority());
+
+       }    
     }
