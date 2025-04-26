7c6d5afdec8471a99e1a8b8310b68e9fa50eda70
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-26 15:08:06 +0530

    Lecture 95

--- Demo.java
+++ Demo.java
@@ -1,13 +1,26 @@
-@FunctionalInterface
-interface  A{
-    int add(int i, int j);
+class A extends Thread // Now this class is not an ordinary class it is a thread
+ {
+    public void run(){
+
+        for(int i=1;i<=10;i++)
+        System.out.println("Hi");
+    }
+}
+
+class B extends Thread{
+    public void run(){
+
+        for(int i=1;i<=10;i++)
+        System.out.println("Hello");
+    }
 }
 
 public class Demo{
     public static void main(String args[]){
-        A obj = ( i, j) ->   i+j; // if we have one return value then we dont need to add the return statement the return value will be the type of i+j
+        A obj1= new A();
+        B obj2= new B();
 
-        int result= obj.add(5,4);
-        System.out.println(result);
+        obj1.start(); // We need to give start if we want to implement threads and the method name will be run()
+        obj2.start();
        }
     }
