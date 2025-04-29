8587a9f490c26caa254a683506f600732d829e2d
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-04-29 15:26:32 +0530

    Lecture 106

--- Demo.java
+++ Demo.java
@@ -1,10 +1,21 @@
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Demo{
     public static void main(String[] args) {
         List<Integer> nums= Arrays.asList(4,5,7,2,6,3);
+
+        // forEach accepts a Consumer object, where Consumer is an interface
+
+        Consumer<Integer> con= new Consumer<Integer>(){
+            
+            public void accept(Integer n){
+                System.out.println(n);
+            }
+        };
+        
 
         // int sum=0;
         // for(int n: nums){
