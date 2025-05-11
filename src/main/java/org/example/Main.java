bf824662e9d23a5a7abb3c0881576906ab6ec5d3
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-11 17:36:52 +0530

    Lecture 149 Fetching with filter and specific properties

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,11 +24,23 @@ public class Main {
         // select * from laptop where ram = 32
         // in HQL
         // from Laptop where ram = 32
+        String brand= "Asus";
+//        Query query = session.createQuery("from Laptop where brand like 'Asus'");
+        // For strings we have to use like clause
+        // If we want to set dynamic parameters
+//        Query query = session.createQuery("from Laptop where brand like ?1"); //?1 refers to the parameter number so as to avoid confusion when the length of code increases
+        // If we want to fetch a particular field from the database
+        Query query = session.createQuery("select model from Laptop where brand like ?1"); //?1 refers to the parameter number so as to avoid confusion when the length of code increases
 
-        Query query = session.createQuery("from Laptop where ram = 32");
+
+        query.setParameter(1,brand);
+//        Query query = session.createQuery("from Laptop where ram = 32");
 
         // If we want to execute the query
-        List<Laptop> laptops= query.getResultList();
+//        List<Laptop> laptops= query.getResultList();
+        // String is more appropriate when we are fetching only one column from the table
+
+        List<String> laptops= query.getResultList();
 //        Laptop l1= session.get(Laptop.class,3);
 //        System.out.println(l1);
 
