ced2c72db67c0fd1f568fb58f4423ee59c34c018
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-11 17:25:08 +0530

    Lecture 148 Fetching data using HQL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,79 +4,37 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Asus");
-        l1.setModel("Rog");
-        l1.setRam(16);
-
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("Dell");
-        l2.setModel("XPS");
-        l2.setRam(32);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Apple");
-        l3.setModel("Macbook");
-        l3.setRam(8);
-
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("Navin");
-        a1.setTech("Java");
-        Alien a2 = new Alien();
-        a2.setAid(102);
-        a2.setAname("Navin");
-        a2.setTech("Java");
-        a1.setLaptops(Arrays.asList(l1, l2));
-        a2.setLaptops(Arrays.asList(l3));
-
-//        l1.setAliens(Arrays.asList(a1,a3));
-//        l2.setAliens(Arrays.asList(a1,a2));
-//        l3.setAliens(Arrays.asList(a2));
-
-
 
         SessionFactory sf = new Configuration()
                 .configure()
-                .addAnnotatedClass(org.example.Alien.class)
                 .addAnnotatedClass(org.example.Laptop.class)
                 .buildSessionFactory();
 
         Session session = sf.openSession();
+        // If we want to fetch the laptop with ram 32
+        // in sql we will write
+        // select * from laptop where ram = 32
+        // in HQL
+        // from Laptop where ram = 32
 
-        Transaction transaction = session.beginTransaction();
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
+        Query query = session.createQuery("from Laptop where ram = 32");
 
-        session.persist(a1);
-        session.persist(a2);
+        // If we want to execute the query
+        List<Laptop> laptops= query.getResultList();
+//        Laptop l1= session.get(Laptop.class,3);
+//        System.out.println(l1);
 
-        transaction.commit();
-
-
-        // Here it will not fire a new query but look for the same query in the current session if we want the query to fire then we have to use seperate session
-
-//        System.out.println(a5);
+        System.out.println(laptops);
         session.close();
-        Session session1= sf.openSession();
-        Alien a5 = session1.get(Alien.class, 101);
-//        If an entity contains a collection then it will fetch the details related to that entity only
-        // not related to a collection, this is called lazy fetching, if we want the data then we can use
-        // System.out.println() to fetch the data related to the collection
 
-//        System.out.println(a5);
-        session1.close();
         sf.close();
 
 
