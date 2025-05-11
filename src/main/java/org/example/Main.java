f5911d45495e5b232ee81ca9f3d75d5b5285ac08
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-11 12:18:41 +0530

    This is the hibernate module using intellij

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsAge(23);
+        s1.setsName("Aporva");
+        s1.setRollNo(101);
+        System.out.println(s1);
+        }
+    }
