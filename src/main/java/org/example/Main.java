4586b0830b8108b9e2b5020fa20c5c61b99f1dc0
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-11 13:43:56 +0530

    Lecture 136 Refractoring the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,19 +9,29 @@ public class Main {
     public static void main(String[] args) {
 
         Student s1 = new Student();
-        s1.setRollNo(104);
-        s1.setsName("Sam");
-        s1.setsAge(30);
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.example.Student.class);
-        cfg.configure("hibernate.cfg.xml");
-        SessionFactory sf = cfg.buildSessionFactory();
+        s1.setRollNo(105);
+        s1.setsName("Aman");
+        s1.setsAge(23);
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure("hibernate.cfg.xml");
+
+        // If we want to write the above 3 lines in one go then
+
+        SessionFactory sf = new Configuration().addAnnotatedClass(org.example.Student.class).configure().buildSessionFactory();
+
+                // cfg.buildSessionFactory(); // Here either we use try with resources or we can use session.close()
+
         Session session = sf.openSession();
         Transaction transaction = session.beginTransaction();
 
-        session.save(s1);
+//        session.save(s1);
+        // Instead of save() we have to use persist as it follows the JPA conventions
 
+        session.persist(s1);
         transaction.commit();
+        session.close();
+        sf.close();
         System.out.println(s1);
 
     }
