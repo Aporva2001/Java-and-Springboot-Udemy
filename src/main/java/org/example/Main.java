b5f19b90f82af95fb47dba3e7633b650f0f7ceb9
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-11 17:45:59 +0530

    Lecture 150 Get vs Load

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,32 +14,10 @@ public class Main {
                 .buildSessionFactory();
 
         Session session = sf.openSession();
-        // If we want to fetch the laptop with ram 32
-        // in sql we will write
-        // select * from laptop where ram = 32
-        // in HQL
-        // from Laptop where ram = 32
-        String brand= "Asus";
-//        Query query = session.createQuery("from Laptop where brand like 'Asus'");
-        // For strings we have to use like clause
-        // If we want to set dynamic parameters
-//        Query query = session.createQuery("from Laptop where brand like ?1"); //?1 refers to the parameter number so as to avoid confusion when the length of code increases
-        // If we want to fetch a particular field from the database
-        Query query = session.createQuery("select model from Laptop where brand like ?1"); //?1 refers to the parameter number so as to avoid confusion when the length of code increases
-
-
-        query.setParameter(1,brand);
-//        Query query = session.createQuery("from Laptop where ram = 32");
-
-        // If we want to execute the query
-//        List<Laptop> laptops= query.getResultList();
-        // String is more appropriate when we are fetching only one column from the table
-
-        List<String> laptops= query.getResultList();
-//        Laptop l1= session.get(Laptop.class,3);
-//        System.out.println(l1);
-
-        System.out.println(laptops);
+//        Laptop laptop= session.load(Laptop.class,2);
+        // Since the load is deprecated we have to use byId
+        Laptop laptop= session.byId(Laptop.class).getReference(2);
+        System.out.println(laptop);
         session.close();
 
         sf.close();
