906bbd50b6cbede8ac2f9877021b69eb58d0bd2e
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-13 12:41:37 +0530

    Lecture 161 Spring Project

---
+++ Getting started with spring/Spring1/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // If we want to create a container, we have to use an application context, to use application context, we have to use external dependencies of spring
+
+        ApplicationContext context= new ClassPathXmlApplicationContext();
+        //now we can specify the object we want
+        Alien obj= (Alien) context.getBean("alien");
+        // alien refers to the name of the object
+        // here the required type is object but we need the object of type Alien so we have to type cast it
+//        Alien obj = new Alien();
+//        obj.code();
+        }
+    }
