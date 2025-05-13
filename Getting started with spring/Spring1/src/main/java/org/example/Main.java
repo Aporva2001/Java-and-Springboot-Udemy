fdbd5d6c527ff86761772f488ac4205a454ef062
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-13 12:55:27 +0530

    Lecture 162 Spring Bean Configuration xml

--- Getting started with spring/Spring1/src/main/java/org/example/Main.java
+++ Getting started with spring/Spring1/src/main/java/org/example/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
         // If we want to create a container, we have to use an application context, to use application context, we have to use external dependencies of spring
 
-        ApplicationContext context= new ClassPathXmlApplicationContext();
+        ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml"); // Here we have to mention the configuration file
         //now we can specify the object we want
         Alien obj= (Alien) context.getBean("alien");
         // alien refers to the name of the object
         // here the required type is object but we need the object of type Alien so we have to type cast it
 //        Alien obj = new Alien();
-//        obj.code();
+        obj.code();
         }
     }
