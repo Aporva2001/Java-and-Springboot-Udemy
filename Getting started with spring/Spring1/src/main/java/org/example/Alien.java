e68c3c0dfcc25a3231c559718e66280e84560785
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-13 13:22:03 +0530

    Lecture 164 Scopes

--- Getting started with spring/Spring1/src/main/java/org/example/Alien.java
+++ Getting started with spring/Spring1/src/main/java/org/example/Alien.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Alien {
+    int age;
     public Alien(){
         System.out.println("Object Created");
     }
--- Getting started with spring/Spring1/src/main/java/org/example/Main.java
+++ Getting started with spring/Spring1/src/main/java/org/example/Main.java
@@ -8,12 +8,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml"); // The object is created in this line
-        Alien obj= (Alien) context.getBean("alien1");
+        //singleton object is created when the container is loaded
+        // for prototype, object will be created only when we say getBean()
 
+        Alien obj= (Alien) context.getBean("alien1");
+        obj.age=21;
+        System.out.println(obj.age);
         Alien obj2= (Alien) context.getBean("alien1");
-        // the object will be created 2 time
+        // the object will be created 1 time as even though we have two different references the object is created one time
+        System.out.println(obj2.age);
         Laptop obj1 = (Laptop) context.getBean("lap");
 //        Alien obj = new Alien();
-        obj.code();
+//        obj.code();
         }
     }
