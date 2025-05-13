a927a8db50790c3fc77f26f2dadbf306fd33a51e
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-13 14:05:50 +0530

    Lecture 167 Constructor injection

--- Getting started with spring/Spring1/src/main/java/org/example/Alien.java
+++ Getting started with spring/Spring1/src/main/java/org/example/Alien.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
     private int age;
     private Laptop lap;
@@ -21,6 +23,12 @@ public class Alien {
 
     public Alien(){
         System.out.println("Object Created");
+    }
+@ConstructorProperties({"age","lap"})
+    public Alien(int age,Laptop lap){
+        System.out.println("Parameterised constructor is called");
+        this.age=age;
+        this.lap= lap;
     }
     public  void code(){
         System.out.println("Coding");
