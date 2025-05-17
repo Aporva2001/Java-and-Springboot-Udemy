a6f7401d8f65918b5f6dbacda4e5ce91abca1ec7
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-17 14:43:26 +0530

    Lecture 199 Responding to the client

--- ServletEx/src/main/java/org/example/HelloServlet.java
+++ ServletEx/src/main/java/org/example/HelloServlet.java
@@ -3,11 +3,24 @@ package org.example;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
-    // This method will be executed whenever we get a request
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
+
+    // If we want to send the get request
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+
+        // If we want to set the type of response to be sent to the server
+
+        res.setContentType("text/html");
+
+        // This is the better way of writing the response to the client
+
+        PrintWriter out= res.getWriter();
+        // we can also mention html tags inside it
+        out.println("<h1>Hello world</h1>");
     }
 }
