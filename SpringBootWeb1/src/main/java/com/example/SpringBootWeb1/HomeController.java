8e1fde6d434c34d26174a733372b88fc095878e1
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-17 19:06:35 +0530

    Lecture 204 Request Mapping

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // If we want to tell spring that this is a controller
 @Controller
 public class HomeController {
+    // If we want to map the request to the home page
+    @RequestMapping("/")
     public String home(){
         System.out.println("Home method called");
         return "index.jsp";
