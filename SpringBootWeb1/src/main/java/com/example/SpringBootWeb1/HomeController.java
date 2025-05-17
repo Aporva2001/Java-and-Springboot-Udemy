891a46da13f4897998730e36805203abe080556c
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-17 19:35:08 +0530

    Lecture 206: Accepting data the servlet way

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +12,17 @@ public class HomeController {
     public String home(){
         System.out.println("Home method called");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+        // The servlet way of getting the data from the request
+
+        int num1 = Integer.parseInt(req.getParameter("num1")); // It will return a string
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println("in add");
+        return "result.jsp";
     }
 }
