b31056c02e89aca6265efb23af37a78f9cede594
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-18 14:39:08 +0530

    Lecture 227 View data

--- JobApp/src/main/java/com/example/JobApp/model/JobPost.java
+++ JobApp/src/main/java/com/example/JobApp/model/JobPost.java
@@ -1,13 +1,12 @@
 package com.example.JobApp.model;
 
+
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-// Since we are using lombok we do not have to write the code for getters and setters and for constructors
 
 @Data
 @NoArgsConstructor
