281e9d95a7650a8d81a8626c42a3679347be316f
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-18 11:06:59 +0530

    Lecture 209: Add model object

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -3,32 +3,27 @@ package com.example.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// If we want to tell spring that this is a controller
 @Controller
 public class HomeController {
     // If we want to map the request to the home page
     @RequestMapping("/")
     public String home(){
         System.out.println("Home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
     // If we want to use different variable name than the query params
-    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, HttpSession session){
-        // This code is working without HttpServlet but we have to make sure that the name in query params and the name
-        // given here is same
-        // public String add(int num1,int num2, HttpSession session)
-
-
+    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
         int result = num + num2;
 
-        session.setAttribute("result",result);
+        model.addAttribute("result",result);
         System.out.println(result);
         System.out.println("in add");
-        return "result.jsp";
+        return "result";
     }
 }
