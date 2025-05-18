973740ebb5f41abfcac776ece3bc723102705731
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-18 11:21:21 +0530

    Lecture 211 Model and View

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -18,12 +19,14 @@ public class HomeController {
 
     @RequestMapping("add")
     // If we want to use different variable name than the query params
-    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2") int num2, ModelAndView mv){
         int result = num + num2;
 
-        model.addAttribute("result",result);
+        mv.addObject("result",result);
         System.out.println(result);
         System.out.println("in add");
-        return "result";
+        mv.setViewName("result");
+
+        return mv;
     }
 }
