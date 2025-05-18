f75fe6b672db3ce6b4e0ddf010452431ce58480e
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-18 14:42:36 +0530

    Section 14 building a project completed

--- JobApp/src/main/java/com/example/JobApp/controller/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/controller/JobController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
 @Controller
-public class JobController {
+public class JobController{
 
     @Autowired
     private JobService service;
