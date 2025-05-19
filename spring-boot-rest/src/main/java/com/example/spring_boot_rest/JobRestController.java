77ce6cac390d184b9471dbdda1a66f64e458d426
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-05-19 13:34:55 +0530

    Connecting react and spring

--- spring-boot-rest/src/main/java/com/example/spring_boot_rest/JobRestController.java
+++ spring-boot-rest/src/main/java/com/example/spring_boot_rest/JobRestController.java
@@ -4,6 +4,7 @@ import com.example.spring_boot_rest.model.JobPost;
 import com.example.spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import java.util.List;
 //@Controller
 // by default the controller assumes that we are returning the jsp data
 @RestController
+// If we want to allow the cross origin cors policy
+@CrossOrigin("http://localhost:3000/")
 public class JobRestController {
 
     @Autowired
